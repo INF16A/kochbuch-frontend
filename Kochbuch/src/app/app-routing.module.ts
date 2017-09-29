11acdd42176d2484cf2e0b93037c608daeb8213b
Thomas Hörner <tho@mainmetall.de>
2017-09-29 08:51:06 +0200

    add routing

--- Kochbuch/src/app/app-routing.module.ts
+++ Kochbuch/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {RezeptansichtComponent} from "./rezeptansicht/rezeptansicht.component";
+import {RezeptanlegenComponent} from "./rezeptanlegen/rezeptanlegen.component";
 
 const routes: Routes = [
   {
     path: '',
     children: []
   },
-  {path: 'rezeptansicht', component: RezeptansichtComponent}
+  {path: 'rezeptansicht', component: RezeptansichtComponent},
+  {path: 'rezeptanlegen', component: RezeptanlegenComponent}
 ];
 
 @NgModule({
