4a1bbbbc53cbae63fb6406fa00d116a120eef9bf
theresa2403 <theresa.reus@web.de>
2017-10-08 18:14:39 +0200

    Library für Social Buttons hinzugefügt, muss nochmal angepasst werden Skripte (Bootstrap, Bootstrap Social etc) über npm installiert Modal wird nun mithilfe von NG-Bootstrap geöffnet eigene Registrierungskomponente mithilfe von Form-Groups einzelne Zeilen in der Form zusammengefasst Radio-Buttons und Checkboxes mit Bootstrap gestylt

--- Kochbuch/src/app/app.module.ts
+++ Kochbuch/src/app/app.module.ts
@@ -4,14 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import { RezeptansichtComponent } from './rezeptansicht/rezeptansicht.component';
 import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import { RegistrierungsmodalComponent } from './registrierungsmodal/registrierungsmodal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RezeptansichtComponent
+    RezeptansichtComponent,
+    RegistrierungsmodalComponent
   ],
   imports: [
     BrowserModule,
