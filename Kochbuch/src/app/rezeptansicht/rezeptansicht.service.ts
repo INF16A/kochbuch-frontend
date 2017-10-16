b10cc3418e7bfde1114fe04d3ecc4e824c48de93
alexkrieg <alexkrieg93@googlemail.com>
2017-10-16 15:27:47 +0200

    Verlinkung, Update Rezeptansichtservice
    Verlinkung Profil & Neues Rezept
    Rezeptansichtservice update für Spring Service 💩=> Alexander Krieg

--- Kochbuch/src/app/rezeptansicht/rezeptansicht.service.ts
+++ Kochbuch/src/app/rezeptansicht/rezeptansicht.service.ts
@@ -1,10 +1,17 @@
 import {Injectable} from '@angular/core';
 import {Observable} from "rxjs";
+import {Http, Response } from '@angular/http';
+
+//💩
+export class Comment{
+  constructor(public creationDate:Date) {}
+}
+//💩
 
 @Injectable()
 export class RezeptansichtService {
 
-  constructor() {
+  constructor(private http:Http) {
   }
 
   /*mockRecipe = {
@@ -71,5 +78,24 @@ export class RezeptansichtService {
   getRecipeData(recipeId) {
     return this.getMockRecipeData();
   }
+
+
+  // 💩
+  /**
+   * Alle Kommentare zu einem Rezept.
+   * @param recipeId 
+   * @param callback 
+   */
+  public getRecipeComments(recipeId:Number, callback: (ar:Comment[]) => void){
+    // this.fetchRecipeComments().subscribe((res:Response) => {
+    //   callback([]);
+    // });
+    callback([]);
+  }
+  private fetchRecipeComments(){
+    let url = "http://";
+    return this.http.get(url);
+  }
+  // 💩
 
 }
