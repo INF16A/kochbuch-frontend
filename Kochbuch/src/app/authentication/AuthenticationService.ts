c40fa1af2b492a524d80e2774069f2197c51da7d
arm.beck.16 <arm.beck.16@lehre.mosbach.dhbw.de>
2017-11-05 05:36:35 +0100

    authentification message handling modified

--- Kochbuch/src/app/authentication/AuthenticationService.ts
+++ Kochbuch/src/app/authentication/AuthenticationService.ts
@@ -1,6 +1,6 @@
 import { Subject } from "rxjs/Subject";
 import { BehaviorSubject } from "rxjs/BehaviorSubject";
-import { HttpInterceptor, HttpRequest ,HttpHandler} from "@angular/common/http";
+import { HttpInterceptor, HttpRequest, HttpHandler } from "@angular/common/http";
 import { User } from '../user.model';
 /**
  * @author Patrick Hahn
@@ -15,7 +15,7 @@ export class AuthenticationService implements HttpInterceptor {
 	public constructor() {
 		this.authenticated = new BehaviorSubject(false);
 	}
-	public intercept(req: HttpRequest<any>,next: HttpHandler) {
+	public intercept(req: HttpRequest<any>, next: HttpHandler) {
 		//TODO this 'if' might has to be replaced
 		if (this.token) {
 			if (req.headers.has("X-Token")) {
@@ -27,23 +27,35 @@ export class AuthenticationService implements HttpInterceptor {
 		}
 		return next.handle(req);
 	}
-	public tryAuthentification(username: string, password: string) {
-		let z = new Promise((resolve, reject) => {
-			const backendLoginUrl = "https://localhost:8080/login";
-			let xmp = new XMLHttpRequest();
-			xmp.open("POST", backendLoginUrl, true);
-			xmp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-			xmp.onload = () => {
-				if (xmp.status >= 200 && xmp.status < 300) {
-					xmp.getResponseHeader("X-Token");
-					resolve(xmp.response);
-				} else {
-					reject(xmp.statusText);
-				}
-			};
-			xmp.onerror = () => reject(xmp.statusText);
-			xmp.send(`username=${encodeURI(username)}&password=${encodeURI(password)}`);
-		});
+	public async tryAuthentification(username: string, password: string) {
+		try {
+			let z: any = await new Promise((resolve, reject) => {
+				const backendLoginUrl = "https://localhost:8080/login";
+				let xmp = new XMLHttpRequest();
+				xmp.open("POST", backendLoginUrl, true);
+				xmp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+				xmp.onload = () => {
+					if (xmp.status >= 200 && xmp.status < 300) {
+						this.token = xmp.getResponseHeader("X-Token");
+						resolve(xmp.response);
+					} else {
+						reject(xmp.statusText);
+					}
+				};
+				xmp.onerror = () => reject(xmp.statusText);
+				xmp.send(`username=${encodeURI(username)}&password=${encodeURI(password)}`);
+			});
+			if (z.id && z.username) {
+				this.currentUser = new User(z.id);
+				this.currentUser.username = z.username;
+			}
+			else {
+				throw new Error("insufficient data loaded");
+			}
+		}
+		catch (e) {
+			throw e;
+		}
 	}
 
 	public debugSetLogin(loggedIn: boolean): void {
