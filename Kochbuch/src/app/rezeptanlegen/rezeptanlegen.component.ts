eedb1beb67b7dc73809e35a298035412c51ae88f
Thomas <hoernerth@gmail.com>
2017-11-12 20:56:33 +0100

    bugfix

--- Kochbuch/src/app/rezeptanlegen/rezeptanlegen.component.ts
+++ Kochbuch/src/app/rezeptanlegen/rezeptanlegen.component.ts
@@ -8,6 +8,7 @@ import {AddingredientmodalComponent} from "../ingredient/addingredientmodal/addi
 import {IngredientService} from "../ingredient/ingredient.service";
 import {Router} from "@angular/router";
 import {AuthenticationService} from "../authentication/AuthenticationService";
+import {IngredientSmall} from "./rezeptanlegen.model";
 
 /**
  * @author Thomas Hörner
@@ -125,7 +126,7 @@ export class RezeptanlegenComponent {
    * Tags
    */
 
-  private needNewTag: boolean = false;
+  private newTagPossible: boolean = false;
 
   removeTag(indexToRemove: number) {
     this.recipeFormTagArray.removeAt(indexToRemove);
@@ -141,10 +142,10 @@ export class RezeptanlegenComponent {
           } else {
             let temp = this._tss.search(term)
             temp.then(value => {
-              if (value.length == 0) {
-                this.needNewTag = true;
+              if (value.length == 0 || (value.length > 0 && value.filter((x) => x.name.toLowerCase() == term.toLowerCase()).length == 0)) {
+                this.newTagPossible = true;
               } else {
-                this.needNewTag = false;
+                this.newTagPossible = false;
               }
               return value;
             });
@@ -153,7 +154,7 @@ export class RezeptanlegenComponent {
         }
       )
       .catch(() => {
-        this.needNewTag = true;
+        this.newTagPossible = true;
         return Observable.of([]);
       });
 
@@ -197,7 +198,7 @@ export class RezeptanlegenComponent {
    * Ingredients
    */
 
-  private needNewIngredient: boolean = false;
+  private newIngredientPossible: boolean = false;
 
   removeIngredient(indexToRemove: number) {
     this.recipeFormIngredientsArray.removeAt(indexToRemove);
@@ -213,10 +214,10 @@ export class RezeptanlegenComponent {
           } else {
             let temp = this._iss.search(term)
             temp.then(value => {
-              if (value.length == 0) {
-                this.needNewIngredient = true;
+              if (value.length == 0 || (value.length > 0 && value.filter((x) => x.ingredientName.toLowerCase() == term.toLowerCase()).length == 0)) {
+                this.newIngredientPossible = true;
               } else {
-                this.needNewIngredient = false;
+                this.newIngredientPossible = false;
               }
               return value;
             });
@@ -225,7 +226,7 @@ export class RezeptanlegenComponent {
         }
       )
       .catch(() => {
-        this.needNewIngredient = true;
+        this.newIngredientPossible = true;
         return Observable.of([]);
       });
 
