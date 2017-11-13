49fe42feec2b358efeae9b32bef8138b25702162
arm.beck.16 <arm.beck.16@lehre.mosbach.dhbw.de>
2017-11-13 03:30:57 +0100

    top 3 rezepte created

--- Kochbuch/src/app/home/home.component.ts
+++ Kochbuch/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { RecipeServie, Recipe } from "../alle-rezepte/alle-rezepte.service";
+import { Recipe } from "../alle-rezepte/alle-rezepte.service";
+import { RezeptService } from "app/RezeptService/rezept.service";
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,28 +8,23 @@ import { RecipeServie, Recipe } from "../alle-rezepte/alle-rezepte.service";
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private RecipeService: RecipeServie) { }
-  private suchtext : string;
+  /**
+ * @author Patrick Hahn
+ * @author Armin Beck
+ * @author Leandro Späth
+ */
+  constructor(private RezeptService: RezeptService) { }
+  private suchtext: string;
 
 
-  Rezepte: Recipe[];
   NewestRecipes: Recipe[];
   BestRecipes: Recipe[];
   ngOnInit() {
-    this.RecipeService.getAllRecipes(ar => {
-      this.Rezepte = ar
-      this.NewestRecipes = this.getNewestRecipes();
-      this.BestRecipes = this.getBestRecipes();
-
+    this.RezeptService.getNewest3().subscribe(ar => {
+      this.NewestRecipes = ar;
+    });
+    this.RezeptService.getTop3().subscribe(ar => {
+      this.BestRecipes = ar;
     });
   }
-  getNewestRecipes() {
-    //get the newest 6 recipes
-    return this.Rezepte.sort((a, b) => a.createDate.valueOf() - b.createDate.valueOf()).slice(0, 6);
-  }
-  getBestRecipes() {
-    // get the 6 most commented recipes
-    return this.Rezepte.sort((a, b) => a.comments.length - b.comments.length).slice(0, 6);
-  }
-
 }
